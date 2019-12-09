7fab785e30032e51ee1a821ce0439648833d950d
Dan Ochs <dochs@ochsholdings.com>
2019-12-09 14:43:09 -0600

    display quiz questions from some json

---
+++ scripts/test_readquiz.js
@@ -0,0 +1,58 @@
+quiz_json = `{
+    "response_code": "0",
+    "results": [
+    {
+    "category": "Entertainment: Music",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which M83 album is the song &quot;Midnight City&quot; featured in?",
+    "correct_answer": "Hurry Up, We&#039;re Dreaming",
+    "incorrect_answers": [
+    "Saturdays = Youth",
+    "Before the Dawn Heals Us",
+    "Junk"
+    ]
+    },
+    {
+    "category": "Science: Computers",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Which one of these is not an official development name for a Ubuntu release?",
+    "correct_answer": "Mystic Mansion",
+    "incorrect_answers": [
+    "Trusty Tahr",
+    "Utopic Unicorn",
+    "Wily Werewolf"
+    ]
+    },
+    {
+    "category": "History",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "In what dialogue did Socrates defend himself to the court of Athens? ",
+    "correct_answer": "The Apology",
+    "incorrect_answers": [
+    "The Euthyphro",
+    "The Laws",
+    "The Republic"
+    ]
+    }
+    ]
+    }`
+
+let myquiz = readquiz('A quiz', 'Dan', quiz_json);
+let qnum = 0;
+displayQuestion(myquiz, qnum);
+//console.log(`displaying ${qnum}`)
+let id = setInterval(func, 3000);
+
+function func() {
+    qnum += 1
+    if (qnum == myquiz.questions.length) {
+        clearInterval(id);
+    }
+    else {
+        displayQuestion(myquiz, qnum);
+        //console.log(`displaying ${qnum}`)
+    }
+}
